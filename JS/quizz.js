e8d8183af307c1e3aef7783e0ea5d1e22ab5ff60
RaiRibPin <raissarp470717@gmail.com>
2024-10-13 17:52:46 -0300

    updateAdivinhacaoImgs

--- JS/quizz.js
+++ JS/quizz.js
@@ -1,31 +1,31 @@
-var pergunta=[];
+var pergunta = [];
 
-    pergunta[0] = "Qual material mais resistente do Minecraft?";
-    pergunta[1] = " Qual mob explode no Minecraft?";
-    pergunta[2] = " Quantos blocos de altura tem um jogador?";
-    pergunta[3] = "O que você obtém ao minerar Redstone?";
-    pergunta[4] = "Qual o nome do dragão no Minecraft?";
-    pergunta[5] = "Quantos corações um jogador tem no Minecraft?";
-    pergunta[6] = "Qual o nome do criador original do Minecraft?";
-    pergunta[7] = "Qual minério é necessário para construir um portal do Nether?";
-    pergunta[8] = "Em que ano o Minecraft foi lançado?";
-    pergunta[9] = "Qual item é necessário para derrotar o Ender Dragon?";
-    pergunta[10] = " Quantos tipos de madeira existem no Minecraft?";
-    pergunta[11] = " Qual mob pode ser domesticado?";
-    pergunta[12] = " Qual mob pode ser domesticado?";
-    pergunta[13] = "Qual é o modo mais difícil do jogo?";
-    pergunta[14] = "Qual o nome do bioma mais comum no Minecraft?";
-    pergunta[15] = "Qual mob pode ser encontrado no Nether?";
-    pergunta[16] = "Qual mob voa no jogo?";
-    pergunta[17] = "Qual mob queima ao sol?";
-    pergunta[18] = "Qual bloco é necessário para encantar itens?";
-    pergunta[19] = "Qual mob te ataca quando você olha diretamente para ele?";
-    pergunta[20] = ". Qual animal no Minecraft pode ser atraído com cenouras e pode ser montado com uma sela?";
-    
+pergunta[0] = "Qual material mais resistente do Minecraft?";
+pergunta[1] = " Qual mob explode no Minecraft?";
+pergunta[2] = " Quantos blocos de altura tem um jogador?";
+pergunta[3] = "O que você obtém ao minerar Redstone?";
+pergunta[4] = "Qual o nome do dragão no Minecraft?";
+pergunta[5] = "Quantos corações um jogador tem no Minecraft?";
+pergunta[6] = "Qual o nome do criador original do Minecraft?";
+pergunta[7] = "Qual minério é necessário para construir um portal do Nether?";
+pergunta[8] = "Em que ano o Minecraft foi lançado?";
+pergunta[9] = "Qual item é necessário para derrotar o Ender Dragon?";
+pergunta[10] = " Quantos tipos de madeira existem no Minecraft?";
+pergunta[11] = " Qual mob pode ser domesticado?";
+pergunta[12] = " Qual mob pode ser domesticado?";
+pergunta[13] = "Qual é o modo mais difícil do jogo?";
+pergunta[14] = "Qual o nome do bioma mais comum no Minecraft?";
+pergunta[15] = "Qual mob pode ser encontrado no Nether?";
+pergunta[16] = "Qual mob voa no jogo?";
+pergunta[17] = "Qual mob queima ao sol?";
+pergunta[18] = "Qual bloco é necessário para encantar itens?";
+pergunta[19] = "Qual mob te ataca quando você olha diretamente para ele?";
+pergunta[20] = ". Qual animal no Minecraft pode ser atraído com cenouras e pode ser montado com uma sela?";
 
-    document.write('<ol>');
-    document.write('<li>'+pergunta[0]+'</li>');
 
-    document.write('<form name="formQuizz"> ');
+document.write('<ol>');
+document.write('<li>' + pergunta[0] + '</li>');
 
-    document.write('<INPUT TYPE="RADIO" NAME="r0" VALUE="Madeira">Madeira </br>');
+document.write('<form name="formQuizz"> ');
+
+document.write('<INPUT TYPE="RADIO" NAME="r0" VALUE="Madeira">Madeira </br>');
